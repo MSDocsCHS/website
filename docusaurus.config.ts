db1a4d8b6bc1eaa17135219ddecaf8b557d29ea2
Caozhi Li <admin@licaoz.com>
2024-11-02 15:42:01 +0000

    feat(config): add organization and project details for deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,6 +11,9 @@ const config: Config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
+  organizationName: 'MSDocsCHS',
+  projectName: 'website',
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
