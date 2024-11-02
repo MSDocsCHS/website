dd6d1fd71574fdbabd7a2ab0af442827523dd464
Caozhi Li <admin@licaoz.com>
2024-11-02 16:21:48 +0000

    feat(deploy): add GitHub Actions workflows for deployment and testing

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,7 +7,7 @@ const config: Config = {
   tagline: 'Microsoft 文档的非官方简体中文译本',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://msdc.licaoz.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
