fcf04710cef6d9923f46efc9ba2700120e0a14c4
Caozhi Li <admin@licaoz.com>
2024-11-02 17:30:43 +0000

    fix(config): change behavior for broken links from 'throw' to 'warn'

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   projectName: 'website',
   deploymentBranch: 'gh-pages',
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
